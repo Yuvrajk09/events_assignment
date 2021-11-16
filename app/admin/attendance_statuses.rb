6fa992299c0f334158c1a4f4cd118a38002038f2
anonymous anonymous <yuvraj.karan@kellogg.northwestern.edu>
2021-11-16 00:56:04 +0000

    Whitelist columns in AttendanceStatus for ActiveAdmin

--- app/admin/attendance_statuses.rb
+++ app/admin/attendance_statuses.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register AttendanceStatus do
 
+ permit_params :attendance_id, :status
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
