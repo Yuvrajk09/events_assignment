56f6227deae11d781c5ab3ac8508383d8786d460
anonymous anonymous <yuvraj.karan@kellogg.northwestern.edu>
2021-11-16 00:56:31 +0000

    Generate graphiti resources for user_details

---
+++ app/resources/user_detail_resource.rb
@@ -0,0 +1,15 @@
+class UserDetailResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :user_id, :integer
+  attribute :user_email, :string
+  attribute :user_first_name, :string
+  attribute :password, :string
+  attribute :user_last_name, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
